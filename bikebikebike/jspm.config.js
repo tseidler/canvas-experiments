5e0bdb11b7f8275c8c38d6a8cf8ae0ad1c1b476d
Terry Seidler <terry.seidler@ogd.nl>
2016-06-19 20:30:01 +0200

    initial commit

---
+++ bikebikebike/jspm.config.js
@@ -0,0 +1,34 @@
+SystemJS.config({
+  paths: {
+    "npm:": "jspm_packages/npm/",
+    "bikebikebike/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.12"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "bikebikebike": {
+      "main": "bikebikebike.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
