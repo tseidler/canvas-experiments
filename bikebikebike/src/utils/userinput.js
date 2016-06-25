48b099525ee6d7fa74dc0b1d117ab027c6d8cdf1
Terry Seidler <terry.seidler@ogd.nl>
2016-06-25 23:54:29 +0200

    Add basic implementation of userinput

--- bikebikebike/src/utils/userinput.js
+++ bikebikebike/src/utils/userinput.js
@@ -3,5 +3,35 @@ export default class UserInput {
     this.keyspressed = {};
 
     // set keydown, keyup event listeners
+    document.addEventListener('keydown', (e) => this.setUserInput(e.keyCode, true));
+    document.addEventListener('keyup', (e) => this.setUserInput(e.keyCode, false));
+  }
+
+  setUserInput(keycode, pressed) {
+    let keyname;
+    switch(keycode) {
+      case 37:
+        keyname = 'left';
+        break;
+      case 39:
+        keyname = 'right';
+        break;
+      case 38:
+        keyname = 'up';
+        break;
+      case 40:
+        keyname = 'down';
+        break;
+      case 32:
+        keyname = 'space';
+        break;
+      default:
+        keyname = keycode;
+    }
+    this.keyspressed[keyname] = pressed;
+  }
+
+  pressed(key = '') {
+    return this.keyspressed.hasOwnProperty(key) && this.keyspressed[key] === true;
   }
 }
